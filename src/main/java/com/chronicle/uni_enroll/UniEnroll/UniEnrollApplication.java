5bd0bfdf55209e8403d324c52dfc817190319369
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-04-14 20:30:07 +0600

    Init

---
+++ src/main/java/com/chronicle/uni_enroll/UniEnroll/UniEnrollApplication.java
@@ -0,0 +1,13 @@
+package com.chronicle.uni_enroll.UniEnroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEnrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniEnrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronicle/uni_enroll/UniEnroll/UniEnrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronicle.uni_enroll.UniEnroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniEnrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
